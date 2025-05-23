28dd035f655ffe69c008c05a08476d4f6b24663a
Katya <kalapasa@bk.ru>
2025-05-23 14:03:40 +0300

    Удалена лишняя строка с подключением скрипта

--- include.php
+++ include.php
@@ -11,7 +11,6 @@ CModule::AddAutoloadClasses($module_id, [
 ]);
 //\Bazarow\DecorateUrSite\EventHandlers\MainEvents::registerHandlers(); <-- не подходит, так как сначала должен быть зарегистрирован модуль. Такая регистрация события подходит для использования в сторонних компонентах, других модулях при предварительном подключении модуля: CModule::IncludeModule('bazarow.decorateursite');
 $APPLICATION->SetAdditionalCSS("/bitrix/css/$module_id/main.css");
-//$APPLICATION->AddHeadScript("/bitrix/css/$module_id/main.js");
 if (COption::GetOptionString($module_id, 'source_script_edit_main') == 'js' && (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') && strpos($_SERVER['REQUEST_URI'], 'bitrix/') === FALSE && php_sapi_name() != "cli"): ?>
 	<?
 	$optValues = \Bazarow\DecorateUrSite\Helper::getAllNeedOptions($module_id);	// get actual parameters (options)
